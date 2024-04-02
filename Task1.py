5304daf883e4646ee9e3669da6dc57675fc9f030
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-02 17:07:08 +0530

    updtae

--- Task1.py
+++ Task1.py
@@ -5,15 +5,22 @@ def odd(a):
         print("the number is odd")
 
 def prime(a):
-    if a%a == 0 and a%2 != 0 :
+    if a%a==0  and a%2 != 0 :
         print("the number is prime number")
     else:
         print("the number is not prime")
+
+def postive(a):
+    if a>0 and a==0 :
+        print("the number is positive ")
+    else:
+        print("the number is negative ")
 
 def main():
     a = int(input("enter number:"))
     odd(a)
     prime(a)
+    postive(a)
 
 if __name__=="__main__":
     main()
