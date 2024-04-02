365ee11dd6a53c018bbc392fb126cfc51969337f
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-02 17:59:56 +0530

    update

--- Task1.py
+++ Task1.py
@@ -5,22 +5,38 @@ def odd(a):
         print("the number is odd")
 
 def prime(a):
-    if a%a==0  and a%2 != 0 :
-        print("the number is prime number")
-    else:
-        print("the number is not prime")
+    if a==1:
+        print("the number is not prime number")
+    elif a>1:
+        for i in range(2,a):
+            if (a%i) == 0:
+                print("the number is not prime number")
+                break
+            else:
+                print("the number is prime")
+                break
+    
 
 def postive(a):
     if a>0 and a==0 :
         print("the number is positive ")
     else:
         print("the number is negative ")
+    
+def palidrome(b):
+    if b == b[::-1]:
+        print("it is palidrome")
+    else:
+        print("not palidrome")
+
 
 def main():
     a = int(input("enter number:"))
-    odd(a)
+    b=input("enter the word :")
+    #odd(a)
     prime(a)
-    postive(a)
+    #postive(a)
+    palidrome(b)
 
 if __name__=="__main__":
     main()
