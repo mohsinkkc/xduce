42f87efb1e57c730b20017f874ad18d27f217d0b
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-02 19:27:56 +0530

    up

--- Task1.py
+++ Task1.py
@@ -22,21 +22,38 @@ def postive(a):
         print("the number is positive ")
     else:
         print("the number is negative ")
-    
+ #======================================== palidrome ==============================================================   
 def palidrome(b):
     if b == b[::-1]:
-        print("it is palidrome")
+        print("word is palidrome")
     else:
-        print("not palidrome")
+        print("word is not palidrome")
 
+#===========================================================================================================
+
+def is_pali(a):
+    temp = a
+    reverse = 0
+    while temp > 0:
+        digit = temp % 10
+        reverse = reverse * 10 + digit
+        temp //= 10
+    return reverse == a
+
+if is_pali(a):
+    print(" it is a palindrome!")
+else:
+    print(" it's not a palindrome.")
+#=================================================================================================================
 
 def main():
     a = int(input("enter number:"))
-    b=input("enter the word :")
+    #b=int(input("enter the word :"))
     #odd(a)
-    prime(a)
+    # prime(a)
     #postive(a)
-    palidrome(b)
+    #palidrome(b)
+    is_pali(a)
 
 if __name__=="__main__":
     main()
