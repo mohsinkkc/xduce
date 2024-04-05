1fd4b21255e6038bf00be013e61b96e66b8b317b
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-05 22:45:08 +0530

    updtae

--- 5th-april test.py
+++ 5th-april test.py
@@ -1,14 +1,11 @@
-'''
-Todays Test:
-Suppose you are building a system to manage student grades. # Implement a Python program that includes the following functionalities:
-1. A function to add a new student with their grades.
-2. A function to calculate the average grade of a student.
-3. A decorator function to ensure that only positive grades are accepted.
-4. A function to retrieve the highest grade among all students.
-5. A function to retrieve the average grade of all students.
-Constraints: - Use dictionaries to store student names as keys and their grades as values.
- - Use sets to ensure that each student's grades are unique
- '''
+# Todays Test:
+# Suppose you are building a system to manage student grades. # Implement a Python program that includes the following functionalities:
+# 1. A function to add a new student with their grades.
+# 2. A function to calculate the average grade of a student.
+# 3. A decorator function to ensure that Student is pass or fail in exam
+# 4. A function to retrieve the highest grade among all students.
+# 5. A function to retrieve the average grade of all students.
+# Constraints: - Use dictionaries to store student names as keys and their grades as values.
 
 #=================================function to add students=========================
 
@@ -43,12 +40,18 @@ def average(student):
             total=sum(student.values())
             avg_grade=total/len(student)
             print("the average is :",avg_grade)
-average(student)
 
 def highest(student):
        high_mark=max(student.values())
        print("the highest grade is :",high_mark)
-highest(student)
+
+
+def main():
+       average(student)
+       highest(student)
+
+if __name__=='__main__':
+       main()
 
 
 
