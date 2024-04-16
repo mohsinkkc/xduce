1f6c31a4d7cf4357e76e90513a457ee6b5baabcc
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-16 22:53:29 +0530

    update

--- 16th-april-OOPs.py
+++ 16th-april-OOPs.py
@@ -10,7 +10,21 @@ class Student:
 s1=Student("mohsin",29)
 print(f"name is :{s1.name} and age : {s1.age}")
 
+#===========================================Calculator task======================================================================================
+class Cal:
+    def add(self):
+        self.n1=int(input("enter the number 1:"))
+        self.n2=int(input("enter the number 2:"))
+        return self.n1+self.n2
+    def sub(self):
+        return self.n1-self.n2
+    def mul(self):
+        return self.n1*self.n2
 
+ob1=Cal()
+print("the addition is :",ob1.add())
+print("the sub is :",ob1.sub())
+print("the multiplication is :",ob1.mul())
 
 #=================================================Data Encapsulation and Abstraction program=============================================================
 class Account:
