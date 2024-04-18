a7297c18a5db8762dfb601d96a51c61bd22be7a6
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-18 18:42:33 +0530

    update

---
+++ week 3/selenium/18th_april.py
@@ -0,0 +1,43 @@
+#pip install selenium
+# pip install webdriver-manager
+#(ChromeDriverManager().install())
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+web = "https://www.investing.com/holiday-calendar/"
+driver = webdriver.Chrome(service=Service((ChromeDriverManager().install())))
+driver.maximize_window()
+
+driver.get(web)
+
+table = driver.find_element(By.ID, "holidayCalendarData")
+rows = table.find_elements(By.TAG_NAME, "tr")
+
+
+columns = []
+data_rows = []
+for row in rows:
+    cells = row.find_elements(By.TAG_NAME, "th") + row.find_elements(By.TAG_NAME, "td")
+    if len(cells) > 0:
+        if not columns:
+            columns = [cell.text.strip() for cell in cells]
+        else:
+            data_rows.append([cell.text.strip() for cell in cells])
+
+pf = pd.DataFrame(data_rows, columns=columns)
+
+
+excel_file='holiday.xlsx'
+pf.to_excel(excel_file,index=False)
+
+print("Your File is extracted succesfully in ",excel_file)
+
+time.sleep(5)
+
+
