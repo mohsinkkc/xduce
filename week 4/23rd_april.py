6849750c32471c61e152b622c0c4505cd2a86807
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-23 18:45:32 +0530

    UP

--- week 4/23rd_april.py
+++ week 4/23rd_april.py
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS student.students (
     subject varchar(60)
 );
 
+
 """)
 cursor.execute("CREATE TABLE IF NOT EXISTS student.teacher (id int primary key, salary int) ")
 
