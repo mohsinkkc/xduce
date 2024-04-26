fdc2643dafa11227a1882a9234f1106d2307e0ec
mohsinkkc <mohsinkkc1@gmail.com>
2024-04-26 17:40:47 +0530

    Changes

--- week 4/test.sql
+++ week 4/test.sql
@@ -1,7 +1,7 @@
 use company;
-
+-- q1 : create Table
 create table if not exists tbEmployee (id int primary key auto_increment,emp_id int ,first_name varchar(80),last_name varchar(70),salary decimal(10,2),joinig_date date,department varchar(80));
-
+-- q2:insert data into table
 INSERT INTO tbEmployee (emp_id, first_name, last_name, salary, joinig_date, department) VALUES 
 (1, 'Mohmad', 'Mohasin', 50000, '2024-05-01', 'Manager'),
 (2, 'Devanshu', 'Chauhan', 75000, '2024-05-02', 'Executive'),
@@ -19,33 +19,41 @@ INSERT INTO tbEmployee (emp_id, first_name, last_name, salary, joinig_date, depa
 (7, 'Dhruv', 'shah', 40000, '2024-05-06', 'Lead');
 
 select*from tbEmployee;
-
+-- Q3 : display name in Upper Case
 select ucase(first_name) from tbEmployee;
-
+-- Q4 : display only first three letter only
 select substring(first_name,1,3) from tbEmployee;
- 
+ -- Q5 :Replace A with a
 select first_name,replace(first_name,'A','a') from tbEmployee;
-
-select * from tbEmployee
+-- Q6 : find the Salary >=5000 and salary <=10000
+select  * from tbemployee
 where salary >=5000 or salary <=10000;
 
-select count(first_name) from tbEmployee
-where department = "admin";
-
-select * from tbEmployee
-where first_name like '%A%';
-
+-- Q7: cloning new table from table
 create table clone_employee like tbEmployee;
 
+-- Q8 : details of worker whose first name contain a
+select * from tbEmployee
+where first_name like '%A%';
+-- Q9 : count of employee working in department admin
+select count(first_name) from tbEmployee
+where department = "admin";
+-- Q10 : current date
+select curdate();
+
+-- Q11 : ten number of highest salary
 select salary from tbEmployee
 order by salary asc limit 10;
-
+-- Q12: create new table from table with its data and structure
 create table clone_employee_data as select * from tbEmployee;
-
+-- Q13 : 3rd higest value
 select salary from tbEmployee
-order by salary limit 3,1;
-
+order by salary limit 2,1;
+-- Q14 : retrive Duplicate value
 select emp_id , count(emp_id) from tbemployee
 group by emp_id
 having count(emp_id)>1;
 
+
+select * from tbemployee
+limit 2;
