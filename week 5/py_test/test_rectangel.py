82e4a57c91b25ba334a95a6a9d7519f9990a3686
mohsinkkc <mohsinkkc1@gmail.com>
2024-05-01 18:24:21 +0530

    update

--- week 5/py_test/test_rectangel.py
+++ week 5/py_test/test_rectangel.py
@@ -1,15 +1,37 @@
 import pytest
-# from py_test.rectangle import Rectangel
-import py_test.rectangle as rectangel
+import time
+import py_test.rectangle as rectangle
+
 
 @pytest.fixture
 def rec():
-    return rectangel.Rectangel(30,40)
+    return rectangle.Rectangel(30,40)
 
 def test_area(rec):
+    # result = rectangle.Rectangel(30,40)
 
-    assert rec.area() == 30 * 40
+    assert rec.area() == 30*40
 
 def test_perimeter(rec):
+    # result=rectangle.Rectangel(30,40) 
 
-    assert rec.perimeter() == (30 * 2) + (40 * 2)
+    assert rec.perimeter() == (30*2) + (40*2)
+
+#if we find any function performing slow we can use @pytest.mark.slow to indicate it's a slow function 
+@pytest.mark.slow
+def test_slow(rec):
+    time.sleep(5)
+    assert rec.perimeter() == (30*2) + (40*2)
+
+#if we have to skip any of the program we use @pytest.mark.skip
+@pytest.mark.skip(reason='this Function is performing very slow and its taking too much time')
+def test_slow(rec):
+    time.sleep(5)
+    assert rec.perimeter() == (30*2) + (40*2)
+
+#if any of the function has not working so we can indicate it's as fail with help of @pytest.mark.xfail
+@pytest.mark.xfail(reason="this function is not working ")
+def test_peri():
+    assert rec.perimeter != (20*2) + (40*2)
+
+
